b5cfd7ca1f97911bfd4b19b0cdf0c684e58311bc
Daniel Parra <jose-paper@hotmail.com>
2022-03-01 10:07:10 -0500

    PlayWright adition

---
+++ playwright.config.ts
@@ -0,0 +1,13 @@
+// playwright.config.ts
+import { PlaywrightTestConfig } from '@playwright/test'
+
+const config: PlaywrightTestConfig = {
+  // Look for test files in the directory, relative to this configuration file
+  testDir: 'src/tests/e2e/',
+  use: {
+    // Configure browser and context here
+    headless: false,
+    browserName: 'firefox',
+  }
+}
+export default config
