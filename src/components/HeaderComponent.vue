bbef2bf9b29fd84074ad22beb9f3f0e792dd1fed
Jose Daniel Parra Gonzalez <jose-paper@hotmail.com>
2022-03-01 14:05:54 -0500

    Header adition

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,9 @@
+<template>
+      <q-header elevated class="glossy">
+      <q-toolbar>
+        <q-toolbar-title>
+          Parking Management
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+</template>
