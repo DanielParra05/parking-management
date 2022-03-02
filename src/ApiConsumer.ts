cacb165b17e3773f2e58cb0eea8a7ab3d225017f
Jose Daniel Parra Gonzalez <jose-paper@hotmail.com>
2022-03-02 15:09:17 -0500

    API implementation

---
+++ src/ApiConsumer.ts
@@ -0,0 +1,24 @@
+import Axios from 'axios'
+import ParkingTicket from './types/ParkingTicket'
+
+const url = 'http://localhost:3000'
+
+function getVehiclesType () {
+  return Axios.get<string[]>(`${url}/vehicle-types`).then((response) => (response.data))
+}
+
+function createParkingTicket (parkingTicket : ParkingTicket) {
+  Axios.post<ParkingTicket>(`${url}/parking-tickets`, parkingTicket)
+}
+/*
+function getParkingTickets (): ParkingTicket[] {
+    const types: ParkingTicket[] = new Array<ParkingTicket>()
+    Axios.get<ParkingTicket[]>(`${url}/parking-tickets`).then((response) => (response.data) = types)
+    return types
+}
+*/
+
+export default {
+  getVehiclesType,
+  createParkingTicket
+}
